7f4e29fcfa1f65213cb348649eb5698c2f14119a
Dan Burkert <dan@danburkert.com>
2017-02-05 13:34:52 -0800

    tweak Message trait

--- src/message.rs
+++ src/message.rs
@@ -7,17 +7,21 @@ use std::io::{
 };
 
 /// A protobuf message.
-pub trait Message: Any + Debug + Default + Send + Sync {
-
-    /// Encode the message and write it to the provided `Write`.
-    fn write_to(&self, w: &mut Write) -> Result<()>;
+pub trait Message: Any + Debug + Send + Sync {
 
     /// Encode the message and its length and write them to the provided `Write`.
-    fn write_length_delimited_to(&self, w: &mut Write) -> Result<()>;
+    fn write_length_delimited_to<W>(&self, w: &mut W) -> Result<()> where W: Write, Self: Sized;
 
-    fn merge_from(&mut self, r: &mut Read) -> Result<()>;
+    /// Merge a length-delimited message into `self`.
+    fn merge_length_delimited_from<R>(&self, r: &mut R) -> Result<()> where R: Read, Self: Sized;
 
-    fn merge_delimited_from(&mut self, r: &mut Read) -> Result<()>;
+    /// Like `write_length_delimited_to`, but allows generic `Message` instances
+    /// (e.g. `&Message`) to be written.
+    fn write_length_delimited_to_dynamic(&self, w: &mut Write) -> Result<()>;
+
+    /// Like `merge_length_delimited_from`, but allows generic `Message` instances
+    /// (e.g. `&mut Message`) to be merged.
+    fn merge_length_delimited_from_dynamic(&mut self, r: &mut Read) -> Result<()>;
 
     fn type_id(&self) -> TypeId;
 
@@ -27,3 +31,7 @@ pub trait Message: Any + Debug + Default + Send + Sync {
 
     fn into_any(self: Box<Self>) -> Box<Any>;
 }
+
+/// Test that the `Message` trait is object-safe.
+#[allow(unused)]
+fn test_message_is_object_safe(message: &Message) {}
