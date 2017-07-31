tokio-rs/prost

commit 05293211f2cc2df6ef384732ab2b1d60d58984c1

Add well-known types, fold prost-codegen into prost-build
* Folds the prost-codegen crate into prost-build.
* Removes the protoc-gen-prost tool. It wasn't being used or tested, and
  prost-build has obviated the need for it. Can always be added back in
  the future if a need arises.
* Adds the prost-types crate, which centralizes definitions for Protobuf
  well-known types.

This commit would have been better split into two, but it snowballed out
of control and I'm not patient enough to go back and separate the
concerns.

fixes danburkert/prost#3
fixes danburkert/prost#8.

This is a breaking change.

prost-build/src/code_generator.rs
@@ -3,13 +3,7 @@ use std::collections::{HashMap, HashSet};
 
 use itertools::{Either, Itertools};
 use multimap::MultiMap;
-
-use ast::{
-    Comments,
-    Method,
-    Service,
-};
-use google::protobuf::{
+use prost_types::{
     DescriptorProto,
     EnumDescriptorProto,
     EnumValueDescriptorProto,
@@ -19,8 +13,14 @@ use google::protobuf::{
     ServiceDescriptorProto,
     SourceCodeInfo,
 };
-use google::protobuf::field_descriptor_proto::{Label, Type};
-use google::protobuf::source_code_info::Location;
+use prost_types::field_descriptor_proto::{Label, Type};
+use prost_types::source_code_info::Location;
+
+use ast::{
+    Comments,
+    Method,
+    Service,
+};
 use ident::{
     camel_to_snake,
     match_field,
@@ -122,6 +122,10 @@ impl <'a> CodeGenerator<'a> {
         // that comments can be retrieved.
         let message_name = message.name.as_ref().expect("message name");
         let fq_message_name = format!(".{}.{}", self.package, message_name);
+
+        // Skip Protobuf well-known types.
+        if self.well_known_type(&fq_message_name).is_some() { return; }
+
         let (nested_types, map_types): (Vec<(DescriptorProto, usize)>, HashMap<String, (FieldDescriptorProto, FieldDescriptorProto)>) =
             message.nested_type.into_iter().enumerate().partition_map(|(idx, nested_type)| {
                 if nested_type.options.as_ref().and_then(|options| options.map_entry).unwrap_or(false) {
@@ -254,9 +258,9 @@ impl <'a> CodeGenerator<'a> {
         self.buf.push_str("pub ");
         self.buf.push_str(&camel_to_snake(field.name()));
         self.buf.push_str(": ");
-        if repeated { self.buf.push_str("Vec<"); }
-        else if optional { self.buf.push_str("Option<"); }
-        if boxed { self.buf.push_str("Box<"); }
+        if repeated { self.buf.push_str("::std::vec::Vec<"); }
+        else if optional { self.buf.push_str("::std::option::Option<"); }
+        if boxed { self.buf.push_str("::std::boxed::Box<"); }
         self.buf.push_str(&ty);
         if boxed { self.buf.push_str(">"); }
         if repeated || optional { self.buf.push_str(">"); }
@@ -312,7 +316,7 @@ impl <'a> CodeGenerator<'a> {
                                    name,
                                    fields.iter().map(|&(ref field, _)| field.number()).join(", ")));
         self.push_indent();
-        self.buf.push_str(&format!("pub {}: Option<{}>,\n", camel_to_snake(oneof.name()), name));
+        self.buf.push_str(&format!("pub {}: ::std::option::Option<{}>,\n", camel_to_snake(oneof.name()), name));
     }
 
     fn append_oneof(&mut self,
@@ -527,7 +531,13 @@ impl <'a> CodeGenerator<'a> {
             Type::TypeBool => Cow::Borrowed("bool"),
             Type::TypeString => Cow::Borrowed("String"),
             Type::TypeBytes => Cow::Borrowed("Vec<u8>"),
-            Type::TypeGroup | Type::TypeMessage => Cow::Owned(self.resolve_ident(field.type_name())),
+            Type::TypeGroup | Type::TypeMessage => {
+                if let Some(ty) = self.well_known_type(field.type_name()) {
+                    Cow::Borrowed(ty)
+                } else {
+                    Cow::Owned(self.resolve_ident(field.type_name()))
+                }
+            },
             Type::TypeEnum => Cow::Borrowed("i32"),
         }
     }
@@ -595,8 +605,65 @@ impl <'a> CodeGenerator<'a> {
             _ => self.syntax == Syntax::Proto2,
         }
     }
+
+    /// Returns the prost_types name for a well-known Protobuf type, or `None` if the provided
+    /// message type is not a well-known type, or prost_types has been disabled.
+    fn well_known_type(&self, fq_msg_type: &str) -> Option<&'static str> {
+        if !self.config.prost_types { return None; }
+        Some(match fq_msg_type {
+            ".google.protobuf.BoolValue" => "bool",
+            ".google.protobuf.UInt32Value" => "u32",
+            ".google.protobuf.UInt64Value" => "u64",
+            ".google.protobuf.Int32Value" => "i32",
+            ".google.protobuf.Int64Value" => "i64",
+            ".google.protobuf.FloatValue" => "f32",
+            ".google.protobuf.DoubleValue" => "f64",
+            ".google.protobuf.StringValue" => "::std::string::String",
+            ".google.protobuf.BytesValue" => "::std::vec::Vec<u8>",
+            ".google.protobuf.Empty" => "()",
+            ".google.protobuf.Duration" => "::prost::types::Duration",
+            ".google.protobuf.Timestamp" => "::prost::types::Timestamp",
+
+            ".google.protobuf.Any" => "::prost_types::Any",
+            ".google.protobuf.Api" => "::prost_types::Api",
+            ".google.protobuf.DescriptorProto" => "::prost_types::DescriptorProto",
+            ".google.protobuf.Enum" => "::prost_types::Enum",
+            ".google.protobuf.EnumDescriptorProto" => "::prost_types::EnumDescriptorProt",
+            ".google.protobuf.EnumOptions" => "::prost_types::EnumOptions",
+            ".google.protobuf.EnumValue" => "::prost_types::EnumValue",
+            ".google.protobuf.EnumValueDescriptorProto" => "::prost_types::EnumValueDescriptorProto",
+            ".google.protobuf.EnumValueOptions" => "::prost_types::EnumValueOptions",
+            ".google.protobuf.Field" => "::prost_types::Field",
+            ".google.protobuf.FieldDescriptorProto" => "::prost_types::FieldDescriptorProto",
+            ".google.protobuf.FieldMask" => "::prost_types::FieldMask",
+            ".google.protobuf.FieldOptions" => "::prost_types::FieldOptions",
+            ".google.protobuf.FileDescriptorProto" => "::prost_types::FileDescriptorProto",
+            ".google.protobuf.FileDescriptorSet" => "::prost_types::FileDescriptorSet",
+            ".google.protobuf.FileOptions" => "::prost_types::FileOptions",
+            ".google.protobuf.GeneratedCodeInfo" => "::prost_types::GeneratedCodeInfo",
+            ".google.protobuf.ListValue" => "::prost_types::ListValue",
+            ".google.protobuf.MessageOptions" => "::prost_types::MessageOptions",
+            ".google.protobuf.Method" => "::prost_types::Method",
+            ".google.protobuf.MethodDescriptorProto" => "::prost_types::MethodDescriptorProto",
+            ".google.protobuf.MethodOption" => "::prost_types::MethodOption",
+            ".google.protobuf.Mixin" => "::prost_types::Mixin",
+            ".google.protobuf.OneofDescriptorProto" => "::prost_types::OneofDescriptorProto",
+            ".google.protobuf.OneofOptions" => "::prost_types::OneofOptions",
+            ".google.protobuf.Option" => "::prost_types::Option",
+            ".google.protobuf.ServiceDescriptorProto" => "::prost_types::ServiceDescriptorProto",
+            ".google.protobuf.ServiceOptions" => "::prost_types::ServiceOptions",
+            ".google.protobuf.SourceCodeInfo" => "::prost_types::SourceCodeInfo",
+            ".google.protobuf.SourceContext" => "::prost_types::SourceContext",
+            ".google.protobuf.Struct" => "::prost_types::Struct",
+            ".google.protobuf.Type" => "::prost_types::Type",
+            ".google.protobuf.UninterpretedOption" => "::prost_types::UninterpretedOption",
+            ".google.protobuf.Value" => "::prost_types::Value",
+            _ => return None,
+        })
+    }
 }
 
+/// Returns `true` if the repeated field type can be packed.
 fn can_pack(field: &FieldDescriptorProto) -> bool {
         match field.type_().expect("unknown field type") {
             Type::TypeFloat   | Type::TypeDouble  | Type::TypeInt32    | Type::TypeInt64    |

prost-build/src/lib.rs
@@ -1,5 +1,9 @@
-//! `prost-build` is a small library which makes it easy to add build-time code generation of
-//! `.proto` files to a Cargo project.
+#![doc(html_root_url = "https://docs.rs/prost-build/0.1.1")]
+
+//! `prost-build` compiles `.proto` files into Rust.
+//!
+//! `prost-build` is designed to be used for build-time code generation as part of a Cargo
+//! build-script.
 //!
 //! ## Example
 //!
@@ -77,14 +81,48 @@
 //!
 //! That's it! Run `cargo doc` to see documentation for the generated code. The full
 //! example project can be found on [GitHub](https://github.com/danburkert/snazzy).
-
-#![doc(html_root_url = "https://docs.rs/prost-build/0.1.1")]
+//!
+//! ## Sourcing `protoc`
+//!
+//! `prost-build` depends on the Protocol Buffers compiler, `protoc`, to parse `.proto` files into
+//! a representation that can be transformed into Rust. If set, `prost_build` will use the `PROTOC`
+//! and `PROTOC_INCLUDE` environment variables for locating `protoc` and the protobuf built-in
+//! includes. For example, on a macOS system where protobuf is installed with Homebrew, set the
+//! environment to:
+//!
+//! ```bash
+//! PROTOC=/usr/local/bin/protoc
+//! PROTOC_INCLUDE=/usr/local/include
+//! ```
+//!
+//! and in a typical Linux installation:
+//!
+//! ```bash
+//! PROTOC=/usr/bin/protoc
+//! PROTOC_INCLUDE=/usr/include
+//! ```
+//!
+//! If `PROTOC` and `PROTOC_INCLUDE` are not found in the environment, then a pre-compiled `protoc`
+//! binary will be downloaded and cached in the target directory. Pre-compiled `protoc` binaries
+//! exist for Linux, macOS, and Windows systems.
 
 extern crate bytes;
+extern crate itertools;
+extern crate multimap;
+extern crate petgraph;
 extern crate prost;
-extern crate prost_codegen;
+extern crate prost_types;
 extern crate tempdir;
 
+#[macro_use]
+extern crate log;
+
+mod ast;
+mod code_generator;
+mod ident;
+mod message_graph;
+
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io::{
@@ -101,10 +139,207 @@ use std::path::{
 use std::process::Command;
 
 use prost::Message;
-use prost_codegen::google::protobuf::FileDescriptorSet;
+use prost_types::{FileDescriptorProto, FileDescriptorSet};
 
 pub const PROTOC: &'static str = env!("PROTOC");
 pub const PROTOC_INCLUDE: &'static str = env!("PROTOC_INCLUDE");
+
+pub use ast::{
+    Comments,
+    Method,
+    Service,
+};
+use code_generator::{
+    CodeGenerator,
+    module,
+};
+use message_graph::MessageGraph;
+
+type Module = Vec<String>;
+
+pub trait ServiceGenerator {
+    fn generate(&self, service: Service, buf: &mut String);
+}
+
+/// Configuration options for Protobuf code generation.
+///
+/// This configuration builder can be used to set non-default code generation options.
+#[derive(Default)]
+pub struct Config {
+    service_generator: Option<Box<ServiceGenerator>>,
+    btree_map: Vec<String>,
+    prost_types: bool,
+}
+
+impl Config {
+
+    /// Creates a new code generator configuration with default options.
+    pub fn new() -> Config {
+        Config::default()
+    }
+
+    /// Configure the code generator to generate Rust [`BTreeMap`][1] fields for Protobuf
+    /// [`map`][2] type fields.
+    ///
+    /// # Arguments
+    ///
+    /// **`paths`** - paths to specific fields, messages, or packages which should use a Rust
+    /// `BTreeMap` for Protobuf `map` fields. Paths are specified in terms of the Protobuf type
+    /// name (not the generated Rust type name). Paths with a leading `.` are treated as fully
+    /// qualified names. Paths without a leading `.` are treated as relative, and are suffix
+    /// matched on the fully qualified field name. If a Protobuf map field matches any of the
+    /// paths, a Rust `BTreeMap` field will be generated instead of the default [`HashMap`][3].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # let mut config = prost_build::Config::new();
+    /// // Match a specific field in a message type.
+    /// config.btree_map(&[".my_messages.MyMessageType.my_map_field"]);
+    ///
+    /// // Match all map fields in a message type.
+    /// config.btree_map(&[".my_messages.MyMessageType"]);
+    ///
+    /// // Match all map fields in a package.
+    /// config.btree_map(&[".my_messages"]);
+    ///
+    /// // Match all map fields.
+    /// config.btree_map(&["."]);
+    ///
+    /// // Match all map fields in a nested message.
+    /// config.btree_map(&[".my_messages.MyMessageType.MyNestedMessageType"]);
+    ///
+    /// // Match all fields named 'my_map_field'.
+    /// config.btree_map(&["my_map_field"]);
+    ///
+    /// // Match all fields named 'my_map_field' in messages named 'MyMessageType', regardless of
+    /// // package or nesting.
+    /// config.btree_map(&["MyMessageType.my_map_field"]);
+    ///
+    /// // Match all fields named 'my_map_field', and all fields in the 'foo.bar' package.
+    /// config.btree_map(&["my_map_field", ".foo.bar"]);
+    /// ```
+    ///
+    /// [1]: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
+    /// [2]: https://developers.google.com/protocol-buffers/docs/proto3#maps
+    /// [3]: https://doc.rust-lang.org/std/collections/struct.HashMap.html
+    pub fn btree_map<I, S>(&mut self, paths: I) -> &mut Self
+    where I: IntoIterator<Item = S>,
+          S: AsRef<str> {
+        self.btree_map = paths.into_iter().map(|s| s.as_ref().to_string()).collect();
+        self
+    }
+
+    /// Configures the code generator to use the provided service generator.
+    pub fn service_generator(&mut self, service_generator: Box<ServiceGenerator>) -> &mut Self {
+        self.service_generator = Some(service_generator);
+        self
+    }
+
+    /// Configures the code generator to not use the `prost_types` crate for Protobuf well-known
+    /// types, and instead generate Protobuf well-known types from their `.proto` definitions.
+    pub fn compile_well_known_types(&mut self) -> &mut Self {
+        self.prost_types = false;
+        self
+    }
+
+    /// Compile `.proto` files into Rust files during a Cargo build with additional code generator
+    /// configuration options.
+    ///
+    /// This method is like the `prost_build::compile_protos` function, with the added ability to
+    /// specify non-default code generation options. See that function for more information about
+    /// the arguments and generated outputs.
+    ///
+    /// # Example `build.rs`
+    ///
+    /// ```norun
+    /// extern crate prost_build;
+    ///
+    /// fn main() {
+    ///     let mut prost_build = prost_build::Config::new();
+    ///     prost_build.btree_map(&["."]);
+    ///     prost_build.compile_protos(&["src/frontend.proto", "src/backend.proto"],
+    ///                                &["src"]).unwrap();
+    /// }
+    /// ```
+    pub fn compile_protos<P>(&self, protos: &[P], includes: &[P]) -> Result<()> where P: AsRef<Path> {
+        let target = match env::var("OUT_DIR") {
+            Ok(val) => PathBuf::from(val),
+            Err(env::VarError::NotPresent) => {
+                return Err(Error::new(ErrorKind::Other,
+                                    "OUT_DIR environment variable is not set"));
+            },
+            Err(env::VarError::NotUnicode(..)) => {
+                return Err(Error::new(ErrorKind::InvalidData,
+                                    "OUT_DIR environment variable is not valid UTF-8"));
+            },
+        };
+
+        // TODO: We should probably emit 'rerun-if-changed=PATH' directives for
+        // cargo, however according to [1] if we output any, those paths will
+        // replace the default crate root, which we don't want. Figure out how to do
+        // it in an additive way, perhaps gcc-rs has this figured out.
+        // [1]: http://doc.crates.io/build-script.html#outputs-of-the-build-script
+
+        let tmp = tempdir::TempDir::new("prost-build")?;
+        let descriptor_set = tmp.path().join("prost-descriptor-set");
+
+        let mut cmd = Command::new(PROTOC);
+        cmd.arg("--include_imports")
+        .arg("--include_source_info")
+        .arg("-o").arg(&descriptor_set);
+
+        for include in includes {
+            cmd.arg("-I").arg(include.as_ref());
+        }
+
+        // Set the protoc include after the user includes in case the user wants to
+        // override one of the built-in .protos.
+        cmd.arg("-I").arg(PROTOC_INCLUDE);
+
+        for proto in protos {
+            cmd.arg(proto.as_ref());
+        }
+
+        let output = cmd.output()?;
+        if !output.status.success() {
+            return Err(Error::new(ErrorKind::Other,
+                                format!("protoc failed: {}",
+                                        String::from_utf8_lossy(&output.stderr))));
+        }
+
+        let mut buf = Vec::new();
+        fs::File::open(descriptor_set)?.read_to_end(&mut buf)?;
+        let descriptor_set = FileDescriptorSet::decode(&buf)?;
+
+        let modules = self.generate(descriptor_set.file);
+        for (module, content) in modules {
+            let mut filename = match module.last() {
+                Some(filename) => PathBuf::from(filename),
+                None => return Err(Error::new(ErrorKind::InvalidInput, ".proto must have a package")),
+            };
+            filename.set_extension("rs");
+            let mut file = fs::File::create(target.join(filename))?;
+            file.write_all(content.as_bytes())?;
+            file.flush()?;
+        }
+
+        Ok(())
+    }
+
+    fn generate(&self, files: Vec<FileDescriptorProto>) -> HashMap<Module, String> {
+        let mut modules = HashMap::new();
+
+        let message_graph = MessageGraph::new(&files);
+
+        for file in files {
+            let module = module(&file);
+            let mut buf = modules.entry(module).or_insert(String::new());
+            CodeGenerator::generate(self, &message_graph, file, &mut buf);
+        }
+        modules
+    }
+}
 
 /// Compile `.proto` files into Rust files during a Cargo build.
 ///
@@ -113,30 +348,6 @@ pub const PROTOC_INCLUDE: &'static str = env!("PROTOC_INCLUDE");
 /// info.
 ///
 /// This function should be called in a project's `build.rs`.
-///
-/// # Sourcing `protoc`
-///
-/// `prost` uses the Protocol Buffers compiler, `protoc`, to parse `.proto` files into a
-/// representation that can be transformed into Rust. If present, `compile_protos` will use the
-/// `PROTOC` and `PROTOC_INCLUDE` environment variables for locating `protoc` and the protobuf
-/// built-in includes. For example, on a macOS system where protobuf is installed with Homebrew,
-/// set the environment to:
-///
-/// ```bash
-/// PROTOC=/usr/local/bin/protoc
-/// PROTOC_INCLUDE=/usr/local/include
-/// ```
-///
-/// and in a typical Linux installation:
-///
-/// ```bash
-/// PROTOC=/usr/bin/protoc
-/// PROTOC_INCLUDE=/usr/include
-/// ```
-///
-/// If `PROTOC` and `PROTOC_INCLUDE` are not found in the environment, then a pre-compiled `protoc`
-/// binary will be downloaded and cached in the target directory. Pre-compiled `protoc` binaries
-/// exist for Linux, macOS, and Windows systems.
 ///
 /// # Arguments
 ///
@@ -173,96 +384,7 @@ pub const PROTOC_INCLUDE: &'static str = env!("PROTOC_INCLUDE");
 /// [2]: http://doc.crates.io/build-script.html#case-study-code-generation
 /// [3]: https://developers.google.com/protocol-buffers/docs/proto3#importing-definitions
 pub fn compile_protos<P>(protos: &[P], includes: &[P]) -> Result<()> where P: AsRef<Path> {
-    compile_protos_with_config(&prost_codegen::Config::new(), protos, includes)
-}
-
-/// Compile `.proto` files into Rust files during a Cargo build with additional code generator
-/// configuration options.
-///
-/// See `prost_codegen::Config` for the available options. Other than providing additional control
-/// over the generated code, this function works identically to `compile_protos`.
-///
-/// # Example `build.rs`
-///
-/// ```norun
-/// extern crate prost_build;
-/// extern crate prost_codegen;
-///
-/// fn main() {
-///     prost_build::compile_protos_with_config(prost_codegen::Config::new().btree_map(&["."]),
-///                                             &["src/frontend.proto", "src/backend.proto"],
-///                                             &["src"]).unwrap();
-/// }
-/// ```
-pub fn compile_protos_with_config<P>(config: &prost_codegen::Config,
-                                     protos: &[P],
-                                     includes: &[P])
-                                     -> Result<()> where P: AsRef<Path> {
-
-    let target = match env::var("OUT_DIR") {
-        Ok(val) => PathBuf::from(val),
-        Err(env::VarError::NotPresent) => {
-            return Err(Error::new(ErrorKind::Other,
-                                  "OUT_DIR environment variable is not set"));
-        },
-        Err(env::VarError::NotUnicode(..)) => {
-            return Err(Error::new(ErrorKind::InvalidData,
-                                  "OUT_DIR environment variable is not valid UTF-8"));
-        },
-    };
-
-    // TODO: We should probably emit 'rerun-if-changed=PATH' directives for
-    // cargo, however according to
-    // http://doc.crates.io/build-script.html#outputs-of-the-build-script if we
-    // output any, those paths will replace the default crate root, which we
-    // don't want. Figure out how to do it in an additive way, perhaps gcc-rs
-    // has this figured out.
-
-
-    let tmp = tempdir::TempDir::new("prost-build")?;
-    let descriptor_set = tmp.path().join("prost-descriptor-set");
-
-    let mut cmd = Command::new(PROTOC);
-    cmd.arg("--include_imports")
-       .arg("--include_source_info")
-       .arg("-o").arg(&descriptor_set);
-
-    for include in includes {
-        cmd.arg("-I").arg(include.as_ref());
-    }
-
-    // Set the protoc include after the user includes in case the user wants to
-    // override one of the built-in .protos.
-    cmd.arg("-I").arg(PROTOC_INCLUDE);
-
-    for proto in protos {
-        cmd.arg(proto.as_ref());
-    }
-
-    let output = cmd.output()?;
-    if !output.status.success() {
-        return Err(Error::new(ErrorKind::Other,
-                              format!("protoc failed: {}",
-                                      String::from_utf8_lossy(&output.stderr))));
-    }
-
-    let mut buf = Vec::new();
-    fs::File::open(descriptor_set)?.read_to_end(&mut buf)?;
-    let descriptor_set = FileDescriptorSet::decode(&buf)?;
-
-    let modules = prost_codegen::generate(config, descriptor_set.file);
-    for (module, content) in modules {
-        let mut filename = match module.last() {
-            Some(filename) => PathBuf::from(filename),
-            None => return Err(Error::new(ErrorKind::InvalidInput, ".proto must have a package")),
-        };
-        filename.set_extension("rs");
-        let mut file = fs::File::create(target.join(filename))?;
-        file.write_all(content.as_bytes())?;
-        file.flush()?;
-    }
-
-    Ok(())
+    Config::new().compile_protos(protos, includes)
 }
 
 #[cfg(test)]
@@ -271,6 +393,6 @@ mod tests {
 
     #[test]
     fn smoke_test() {
-        compile_protos(&["src/smoke_test.proto"], &["src"]).unwrap();
+        Config::new().compile_protos(&["src/smoke_test.proto"], &["src"]).unwrap();
     }
 }

commit f73e0198f76d9ed438277e1072edec6962c21a09

Fix some prost-types bugs
This fixes up some issues introduced by 05293211f2cc2df6ef3:

* Use prost-types by default in prost-build
* Support enum well known types (`NullValue`)

This is a breaking change.

prost-build/src/code_generator.rs
@@ -434,6 +434,10 @@ impl <'a> CodeGenerator<'a> {
     fn append_enum(&mut self, desc: EnumDescriptorProto) {
         debug!("\tenum: {:?}", desc.name());
 
+        // Skip Protobuf well-known types.
+        let fq_enum_name = format!(".{}.{}", self.package, desc.name());
+        if self.well_known_type(&fq_enum_name).is_some() { return; }
+
         self.append_doc();
         self.push_indent();
         self.buf.push_str("#[derive(Clone, Copy, Debug, PartialEq, Eq, Enumeration)]\n");
@@ -631,21 +635,20 @@ impl <'a> CodeGenerator<'a> {
         if !self.config.prost_types { return None; }
         Some(match fq_msg_type {
             ".google.protobuf.BoolValue" => "bool",
-            ".google.protobuf.UInt32Value" => "u32",
-            ".google.protobuf.UInt64Value" => "u64",
+            ".google.protobuf.BytesValue" => "::std::vec::Vec<u8>",
+            ".google.protobuf.DoubleValue" => "f64",
+            ".google.protobuf.Empty" => "()",
+            ".google.protobuf.FloatValue" => "f32",
             ".google.protobuf.Int32Value" => "i32",
             ".google.protobuf.Int64Value" => "i64",
-            ".google.protobuf.FloatValue" => "f32",
-            ".google.protobuf.DoubleValue" => "f64",
             ".google.protobuf.StringValue" => "::std::string::String",
-            ".google.protobuf.BytesValue" => "::std::vec::Vec<u8>",
-            ".google.protobuf.Empty" => "()",
-            ".google.protobuf.Duration" => "::prost::types::Duration",
-            ".google.protobuf.Timestamp" => "::prost::types::Timestamp",
+            ".google.protobuf.UInt32Value" => "u32",
+            ".google.protobuf.UInt64Value" => "u64",
 
             ".google.protobuf.Any" => "::prost_types::Any",
             ".google.protobuf.Api" => "::prost_types::Api",
             ".google.protobuf.DescriptorProto" => "::prost_types::DescriptorProto",
+            ".google.protobuf.Duration" => "::prost_types::Duration",
             ".google.protobuf.Enum" => "::prost_types::Enum",
             ".google.protobuf.EnumDescriptorProto" => "::prost_types::EnumDescriptorProt",
             ".google.protobuf.EnumOptions" => "::prost_types::EnumOptions",
@@ -660,6 +663,7 @@ impl <'a> CodeGenerator<'a> {
             ".google.protobuf.FileDescriptorSet" => "::prost_types::FileDescriptorSet",
             ".google.protobuf.FileOptions" => "::prost_types::FileOptions",
             ".google.protobuf.GeneratedCodeInfo" => "::prost_types::GeneratedCodeInfo",
+            ".google.protobuf.NullValue" => "::prost_types::NullValue",
             ".google.protobuf.ListValue" => "::prost_types::ListValue",
             ".google.protobuf.MessageOptions" => "::prost_types::MessageOptions",
             ".google.protobuf.Method" => "::prost_types::Method",
@@ -674,6 +678,7 @@ impl <'a> CodeGenerator<'a> {
             ".google.protobuf.SourceCodeInfo" => "::prost_types::SourceCodeInfo",
             ".google.protobuf.SourceContext" => "::prost_types::SourceContext",
             ".google.protobuf.Struct" => "::prost_types::Struct",
+            ".google.protobuf.Timestamp" => "::prost_types::Timestamp",
             ".google.protobuf.Type" => "::prost_types::Type",
             ".google.protobuf.UninterpretedOption" => "::prost_types::UninterpretedOption",
             ".google.protobuf.Value" => "::prost_types::Value",

prost-build/src/lib.rs
@@ -122,6 +122,7 @@ mod code_generator;
 mod ident;
 mod message_graph;
 
+use std::default;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
@@ -164,7 +165,6 @@ pub trait ServiceGenerator {
 /// Configuration options for Protobuf code generation.
 ///
 /// This configuration builder can be used to set non-default code generation options.
-#[derive(Default)]
 pub struct Config {
     service_generator: Option<Box<ServiceGenerator>>,
     btree_map: Vec<String>,
@@ -336,6 +336,16 @@ impl Config {
             CodeGenerator::generate(self, &message_graph, file, &mut buf);
         }
         modules
+    }
+}
+
+impl default::Default for Config {
+    fn default() -> Config {
+        Config {
+            service_generator: None,
+            btree_map: Vec::new(),
+            prost_types: true,
+        }
     }
 }
 