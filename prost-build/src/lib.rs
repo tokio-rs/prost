357065043498c65afaf9a97ce17deae056a8a3d8
Brian Smith <brian@briansmith.org>
2018-03-03 17:31:01 -1000

    v0.3.1
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-build/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/prost-build/0.3.1")]
 
 //! `prost-build` compiles `.proto` files into Rust.
 //!
