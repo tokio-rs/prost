6901d3b875347c28a9a186bca932b16021f00cad
Per Grön <pgron@google.com>
2018-07-13 15:34:21 +0200

    Define the data that is to be stored in UnknownFieldSet
    The design is derived from the C++ UnknownFieldSet in the Google
    Protobuf library.

--- src/unknown_field_set.rs
+++ src/unknown_field_set.rs
@@ -7,4 +7,25 @@
 /// which is required by the Protobuf spec.
 #[derive(Clone, Debug, PartialEq, Eq, Default, PartialOrd, Ord)]
 pub struct UnknownFieldSet {
+    // The actual data of this struct is wrapped in a Box to ensure that
+    // this struct uses only one machine word of memory unless there are
+    // unknown fields to store.
+    //
+    // If the Option is non-empty, the Vec is also non-empty.
+    data: Option<Box<Vec<UnknownField>>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct UnknownField {
+    tag: u32,
+    data: UnknownFieldData,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum UnknownFieldData {
+    Varint(u64),
+    SixtyFourBit(u64),
+    LengthDelimited(u64),
+    Group(UnknownFieldSet),
+    ThirtyTwoBit(u32),
 }
