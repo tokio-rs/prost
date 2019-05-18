0c55a070d3d95241c3caafda667de489bef7b109
Dan Burkert <dan@danburkert.com>
2019-05-18 14:05:52 -0700

    fix fuzzers

--- fuzz/fuzzers/proto2.rs
+++ fuzz/fuzzers/proto2.rs
@@ -1,9 +1,10 @@
 #![no_main]
 
+use libfuzzer_sys::fuzz_target;
 use protobuf::test_messages::proto2::TestAllTypesProto2;
 use tests::roundtrip;
 
-libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+fuzz_target!(|data: &[u8]| {
     let _ = roundtrip::<TestAllTypesProto2>(data).unwrap_error();
 });
 
