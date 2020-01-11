7e0070f140f220af1c068c22e8428177b13706bc
Dan Burkert <dan@danburkert.com>
2020-01-11 12:50:12 -0800

    More fallible Try to TryFrom conversions
    This is a breaking change to `prost-types`.

--- prost-types/src/lib.rs
+++ prost-types/src/lib.rs
@@ -9,6 +9,7 @@
 //!
 //! [1]: https://developers.google.com/protocol-buffers/docs/reference/google.protobuf
 
+use std::convert::TryFrom;
 use std::i32;
 use std::i64;
 use std::time;
@@ -71,11 +72,12 @@ impl From<time::Duration> for Duration {
     }
 }
 
-/// Converts a `Duration` to a result containing a positive (`Ok`) or negative (`Err`)
-/// `std::time::Duration`.
-// TODO: convert this to TryFrom when it's stable.
-impl From<Duration> for Result<time::Duration, time::Duration> {
-    fn from(mut duration: Duration) -> Result<time::Duration, time::Duration> {
+impl TryFrom<Duration> for time::Duration {
+    type Error = time::Duration;
+
+    /// Converts a `Duration` to a result containing a positive (`Ok`) or negative (`Err`)
+    /// `std::time::Duration`.
+    fn try_from(mut duration: Duration) -> Result<time::Duration, time::Duration> {
         duration.normalize();
         if duration.seconds >= 0 {
             Ok(time::Duration::new(
@@ -126,11 +128,12 @@ impl From<time::SystemTime> for Timestamp {
     }
 }
 
-/// Converts a `Timestamp` to a `SystemTime`, or if the timestamp falls before the Unix epoch, a
-/// duration containing the difference.
-// TODO: convert this to TryFrom when it's stable.
-impl From<Timestamp> for Result<time::SystemTime, time::Duration> {
-    fn from(mut timestamp: Timestamp) -> Result<time::SystemTime, time::Duration> {
+impl TryFrom<Timestamp> for time::SystemTime {
+    type Error = time::Duration;
+
+    /// Converts a `Timestamp` to a `SystemTime`, or if the timestamp falls before the Unix epoch,
+    /// a duration containing the difference.
+    fn try_from(mut timestamp: Timestamp) -> Result<time::SystemTime, time::Duration> {
         timestamp.normalize();
         if timestamp.seconds >= 0 {
             Ok(time::UNIX_EPOCH
