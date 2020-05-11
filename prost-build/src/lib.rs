e3de89e8b3731869b9e9371b0bf2d6b5f8c9cd26
Dan Burkert <dan@danburkert.com>
2020-05-11 10:58:45 -0600

    prost-build: fix compilation error when using --no-default-features

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -551,7 +551,12 @@ impl Config {
         }
 
         let buf = fs::read(descriptor_set)?;
-        let descriptor_set = FileDescriptorSet::decode(&*buf)?;
+        let descriptor_set = FileDescriptorSet::decode(&*buf).map_err(|error| {
+            Error::new(
+                ErrorKind::InvalidInput,
+                format!("invalid FileDescriptorSet: {}", error.to_string()),
+            )
+        })?;
 
         let modules = self.generate(descriptor_set.file)?;
         for (module, content) in modules {
