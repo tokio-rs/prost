ceb34d37dd53e15e7aaa89e13eb0e4121cefcf50
daladim <daladim@users.noreply.github.com>
2020-11-13 18:56:55 +0100

    Added Config::field_visibility

--- prost-build/src/code_generator.rs
+++ prost-build/src/code_generator.rs
@@ -19,6 +19,7 @@ use crate::extern_paths::ExternPaths;
 use crate::ident::{match_ident, to_snake, to_upper_camel};
 use crate::message_graph::MessageGraph;
 use crate::Config;
+use crate::Visibility;
 
 #[derive(PartialEq)]
 enum Syntax {
@@ -388,7 +389,11 @@ impl<'a> CodeGenerator<'a> {
         self.buf.push_str("\")]\n");
         self.append_field_attributes(msg_name, field.name());
         self.push_indent();
-        self.buf.push_str("pub ");
+        let pub_modifier = match self.get_field_visibility(msg_name, field.name()) {
+            Visibility::Public => "pub ",
+            Visibility::Private => "",
+        };
+        self.buf.push_str(pub_modifier);
         self.buf.push_str(&to_snake(field.name()));
         self.buf.push_str(": ");
         if repeated {
@@ -452,8 +457,13 @@ impl<'a> CodeGenerator<'a> {
         ));
         self.append_field_attributes(msg_name, field.name());
         self.push_indent();
+        let pub_modifier = match self.get_field_visibility(msg_name, field.name()) {
+            Visibility::Public => "pub",
+            Visibility::Private => "",
+        };
         self.buf.push_str(&format!(
-            "pub {}: {}::{}<{}, {}>,\n",
+            "{} {}: {}::{}<{}, {}>,\n",
+            pub_modifier,
             to_snake(field.name()),
             lib_name,
             rust_ty,
@@ -486,8 +496,13 @@ impl<'a> CodeGenerator<'a> {
         ));
         self.append_field_attributes(fq_message_name, oneof.name());
         self.push_indent();
+        let pub_modifier = match self.get_field_visibility(fq_message_name, oneof.name()) {
+            Visibility::Public => "pub",
+            Visibility::Private => "",
+        };
         self.buf.push_str(&format!(
-            "pub {}: ::core::option::Option<{}>,\n",
+            "{} {}: ::core::option::Option<{}>,\n",
+            pub_modifier,
             to_snake(oneof.name()),
             name
         ));
@@ -512,7 +527,11 @@ impl<'a> CodeGenerator<'a> {
         self.buf
             .push_str("#[derive(Clone, PartialEq, ::prost::Oneof)]\n");
         self.push_indent();
-        self.buf.push_str("pub enum ");
+        let pub_and_enum = match self.get_field_visibility(msg_name, oneof.name()) {
+            Visibility::Public => "pub enum ",
+            Visibility::Private => "enum ",
+        };
+        self.buf.push_str(pub_and_enum);
         self.buf.push_str(&to_upper_camel(oneof.name()));
         self.buf.push_str(" {\n");
 
@@ -599,7 +618,11 @@ impl<'a> CodeGenerator<'a> {
         self.push_indent();
         self.buf.push_str("#[repr(i32)]\n");
         self.push_indent();
-        self.buf.push_str("pub enum ");
+        let pub_and_name = match self.get_field_visibility(&fq_enum_name, desc.name()) {
+            Visibility::Public => "pub enum ",
+            Visibility::Private => "enum ",
+        };
+        self.buf.push_str(pub_and_name);
         self.buf.push_str(&to_upper_camel(desc.name()));
         self.buf.push_str(" {\n");
 
@@ -853,6 +876,17 @@ impl<'a> CodeGenerator<'a> {
             .options
             .as_ref()
             .map_or(false, FieldOptions::deprecated)
+    }
+
+    fn get_field_visibility(&mut self, msg_name: &str, field_name: &str) -> Visibility {
+        assert_eq!(b'.', msg_name.as_bytes()[0]);
+        // TODO: this clone is dirty, but expedious.
+        for (matcher, visibility) in self.config.field_visibilities.clone() {
+            if match_ident(&matcher, msg_name, Some(field_name)) {
+                return visibility;
+            }
+        }
+        Visibility::Public
     }
 }
 
--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -179,6 +179,14 @@ pub trait ServiceGenerator {
     fn finalize_package(&mut self, _package: &str, _buf: &mut String) {}
 }
 
+#[derive(Copy, Clone, Debug)]
+pub enum Visibility {
+    /// pub
+    Public,
+    /// not pub
+    Private,
+}
+
 /// Configuration options for Protobuf code generation.
 ///
 /// This configuration builder can be used to set non-default code generation options.
@@ -188,6 +196,7 @@ pub struct Config {
     bytes: Vec<String>,
     type_attributes: Vec<(String, String)>,
     field_attributes: Vec<(String, String)>,
+    field_visibilities: Vec<(String, Visibility)>,
     prost_types: bool,
     strip_enum_prefix: bool,
     out_dir: Option<PathBuf>,
@@ -345,6 +354,34 @@ impl Config {
     {
         self.field_attributes
             .push((path.as_ref().to_string(), attribute.as_ref().to_string()));
+        self
+    }
+
+    /// Choose the visibilily of matched fields.
+    ///
+    /// # Arguments
+    ///
+    /// **`path`** - a patch matching any number of fields. These fields get the specified visibility.
+    /// For details about matching fields see [`btree_map`](#method.btree_map).
+    ///
+    /// **`visibility`** - whether the field will be `pub` (the default) or not.
+    ///
+    /// Note that for `optional` proto2 fields, a getter will be generated, which will be `pub` anyway
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # let mut config = prost_build::Config::new();
+    /// // Prost renames fields named `in` to `in_`. But if serialized through serde,
+    /// // they should as `in`.
+    /// config.field_visibility("in", prost_build::Visibility::Private);
+    /// ```
+    pub fn field_visibility<P>(&mut self, path: P, visibility: Visibility) -> &mut Self
+    where
+        P: AsRef<str>,
+    {
+        self.field_visibilities
+            .push((path.as_ref().to_string(), visibility));
         self
     }
 
@@ -715,6 +752,7 @@ impl default::Default for Config {
             bytes: Vec::new(),
             type_attributes: Vec::new(),
             field_attributes: Vec::new(),
+            field_visibilities: Vec::new(),
             prost_types: true,
             strip_enum_prefix: true,
             out_dir: None,
