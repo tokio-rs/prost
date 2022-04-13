28da58c0d9ea7d371462be4ae13e74f876ab844e
Lucio Franco <luciofranco14@gmail.com>
2022-04-13 10:14:39 -0400

    Prepare `0.10.1` release

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-build/0.10.0")]
+#![doc(html_root_url = "https://docs.rs/prost-build/0.10.1")]
 #![allow(clippy::option_as_ref_deref)]
 
 //! `prost-build` compiles `.proto` files into Rust.
--- prost-types/src/lib.rs
+++ prost-types/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-types/0.9.0")]
+#![doc(html_root_url = "https://docs.rs/prost-types/0.10.1")]
 
 //! Protocol Buffers well-known types.
 //!
