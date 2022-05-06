63e1e2b1121a0dfc8262709e3e463faab0cabae8
Jake Shadle <jake.shadle@embark-studios.com>
2022-05-06 09:05:23 +0200

    Fix windows...again

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -1007,7 +1007,7 @@ impl Config {
                 path.as_ref()
                     .as_os_str()
                     .encode_wide()
-                    .flat_map(|c| [((c & 0xff00) >> 8) as u8, (c & 0xff) as u8])
+                    .flat_map(|c| [((c & 0xff00) >> 8) as u8, (c & 0xff) as u8].into_iter())
                     .collect::<Vec<_>>()
                     .into()
             }
