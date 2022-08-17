6a63ace9399e629cdfcba3ae72cde9f1d6a026bb
Jason Hills <jason.hills@vivint.com>
2022-08-17 10:32:56 -0600

    tests with one-of

--- prost-examples/src/lib.rs
+++ prost-examples/src/lib.rs
@@ -4,22 +4,28 @@ pub mod hello_world {
 
 #[cfg(test)]
 mod test {
+    use super::hello_world::new_hello_world::*;
+    use super::hello_world::*;
     use prost::Message;
 
     #[test]
     fn test_hello() {
-        let mut msg = super::hello_world::NewHelloWorld::default();
+        let mut msg = NewHelloWorld::default();
         msg.foo = 32;
         msg.new_field = 45;
+        msg.bar = Some(Bar::Inner1(Inner {
+            cool_string: "really cool".to_string(),
+            ..Inner::default()
+        }));
         let mut buf = Vec::new();
         msg.encode_raw(&mut buf);
 
-        let decoded = super::hello_world::OldHelloWorld::decode(buf.as_slice()).unwrap();
+        let decoded = OldHelloWorld::decode(buf.as_slice()).unwrap();
         println!("{:?}", decoded);
         let mut buf = Vec::new();
         decoded.encode_raw(&mut buf);
 
-        let decoded = super::hello_world::NewHelloWorld::decode(buf.as_slice()).unwrap();
+        let decoded = NewHelloWorld::decode(buf.as_slice()).unwrap();
         assert_eq!(msg, decoded);
     }
 }
