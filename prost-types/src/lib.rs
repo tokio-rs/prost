a4f5992b7c6d91c5eb33accd3d3660016b80ae65
Donough Liu <liudingming@bytedance.com>
2023-04-04 00:04:59 +0800

    Fix corrupted tests and missing CI testing (#832)
    Co-authored-by: Lucio Franco <luciofranco14@gmail.com>

--- prost-types/src/lib.rs
+++ prost-types/src/lib.rs
@@ -423,11 +423,12 @@ impl fmt::Display for Timestamp {
 
 #[cfg(test)]
 mod tests {
-    use std::time::{self, SystemTime, UNIX_EPOCH};
-
-    use proptest::prelude::*;
-
     use super::*;
+
+    #[cfg(feature = "std")]
+    use proptest::prelude::*;
+    #[cfg(feature = "std")]
+    use std::time::{self, SystemTime, UNIX_EPOCH};
 
     #[cfg(feature = "std")]
     proptest! {
