3cf34f00d3b919ce65cc61fc869feba5aeb487e7
Erwan Or <erwan.ounn.84@gmail.com>
2023-11-16 11:11:40 -0500

    prost: prepare release `0.12.2` (#943)

--- prost-derive/src/lib.rs
+++ prost-derive/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-derive/0.12.1")]
+#![doc(html_root_url = "https://docs.rs/prost-derive/0.12.2")]
 // The `quote!` macro requires deep recursion.
 #![recursion_limit = "4096"]
 
