370d0dc88e034402ab80adafce884d4539131016
Tin Rabzelj <tin@flinect.com>
2023-11-17 02:02:07 +0100

    Fix code generation for Name trait

--- prost-build/src/code_generator.rs
+++ prost-build/src/code_generator.rs
@@ -29,6 +29,7 @@ enum Syntax {
 pub struct CodeGenerator<'a> {
     config: &'a mut Config,
     package: String,
+    type_path: Vec<String>,
     source_info: Option<SourceCodeInfo>,
     syntax: Syntax,
     message_graph: &'a MessageGraph,
@@ -69,6 +70,7 @@ impl<'a> CodeGenerator<'a> {
         let mut code_gen = CodeGenerator {
             config,
             package: file.package.unwrap_or_default(),
+            type_path: Vec::new(),
             source_info,
             syntax,
             message_graph,
@@ -83,13 +85,6 @@ impl<'a> CodeGenerator<'a> {
             file.name.as_ref().unwrap(),
             code_gen.package
         );
-
-        if code_gen.config.enable_type_names {
-            code_gen.buf.push_str(&format!(
-                "const PACKAGE: &str = \"{}\";\n",
-                code_gen.package,
-            ));
-        }
 
         code_gen.path.push(4);
         for (idx, message) in file.message_type.into_iter().enumerate() {
@@ -128,10 +123,16 @@ impl<'a> CodeGenerator<'a> {
 
         let message_name = message.name().to_string();
         let fq_message_name = format!(
-            "{}{}.{}",
-            if self.package.is_empty() { "" } else { "." },
-            self.package,
-            message.name()
+            "{}{}{}{}.{}",
+            if self.package.is_empty() && self.type_path.is_empty() {
+                ""
+            } else {
+                "."
+            },
+            self.package.trim_matches('.'),
+            if self.type_path.is_empty() { "" } else { "." },
+            self.type_path.join("."),
+            message_name,
         );
 
         // Skip external types.
@@ -282,19 +283,34 @@ impl<'a> CodeGenerator<'a> {
         ));
         self.depth += 1;
 
-        self.buf
-            .push_str("const PACKAGE: &'static str = PACKAGE;\n");
         self.buf.push_str(&format!(
             "const NAME: &'static str = \"{}\";\n",
-            message_name
+            message_name,
+        ));
+        self.buf.push_str(&format!(
+            "const PACKAGE: &'static str = \"{}\";\n",
+            self.package,
+        ));
+
+        let prost_path = self.config.prost_path.as_deref().unwrap_or("::prost");
+        let string_path = format!("{}::alloc::string::String", prost_path);
+        let format_path = format!("{}::alloc::format", prost_path);
+
+        self.buf.push_str(&format!(
+            r#"fn full_name() -> {string_path} {{
+                {format_path}!("{}{}{}{}{{}}", Self::NAME)
+            }}"#,
+            self.package.trim_matches('.'),
+            if self.package.is_empty() { "" } else { "." },
+            self.type_path.join("."),
+            if self.type_path.is_empty() { "" } else { "." },
         ));
 
         if let Some(domain_name) = self.config.type_name_domains.get_first(fq_message_name) {
             self.buf.push_str(&format!(
-                r#"fn type_url() -> String {{
-                    format!("{}/{{}}", Self::full_name())
+                r#"fn type_url() -> {string_path} {{
+                    {format_path}!("{domain_name}/{{}}", Self::full_name())
                 }}"#,
-                domain_name
             ));
         }
 
@@ -684,11 +700,18 @@ impl<'a> CodeGenerator<'a> {
 
         let enum_values = &desc.value;
         let fq_proto_enum_name = format!(
-            "{}{}.{}",
-            if self.package.is_empty() { "" } else { "." },
-            self.package,
-            proto_enum_name
+            "{}{}{}{}.{}",
+            if self.package.is_empty() && self.type_path.is_empty() {
+                ""
+            } else {
+                "."
+            },
+            self.package.trim_matches('.'),
+            if self.type_path.is_empty() { "" } else { "." },
+            self.type_path.join("."),
+            proto_enum_name,
         );
+
         if self
             .extern_paths
             .resolve_ident(&fq_proto_enum_name)
@@ -906,8 +929,7 @@ impl<'a> CodeGenerator<'a> {
         self.buf.push_str(&to_snake(module));
         self.buf.push_str(" {\n");
 
-        self.package.push('.');
-        self.package.push_str(module);
+        self.type_path.push(module.into());
 
         self.depth += 1;
     }
@@ -915,8 +937,7 @@ impl<'a> CodeGenerator<'a> {
     fn pop_mod(&mut self) {
         self.depth -= 1;
 
-        let idx = self.package.rfind('.').unwrap();
-        self.package.truncate(idx);
+        self.type_path.pop();
 
         self.push_indent();
         self.buf.push_str("}\n");
@@ -954,7 +975,11 @@ impl<'a> CodeGenerator<'a> {
             return proto_ident;
         }
 
-        let mut local_path = self.package.split('.').peekable();
+        let mut local_path = self
+            .package
+            .split('.')
+            .chain(self.type_path.iter().map(String::as_str))
+            .peekable();
 
         // If no package is specified the start of the package name will be '.'
         // and split will return an empty string ("") which breaks resolution
--- tests/src/type_names.rs
+++ tests/src/type_names.rs
@@ -1,4 +1,3 @@
-use prost::alloc::{format, string::String};
 use prost::Name;
 
 include!(concat!(env!("OUT_DIR"), "/type_names.rs"));
@@ -7,9 +6,16 @@ include!(concat!(env!("OUT_DIR"), "/type_names.rs"));
 fn valid_type_names() {
     assert_eq!("Foo", Foo::NAME);
     assert_eq!("type_names", Foo::PACKAGE);
+    assert_eq!("type_names.Foo", Foo::full_name());
     assert_eq!("tests/type_names.Foo", Foo::type_url());
 
-    assert_eq!("Bar", Bar::NAME);
-    assert_eq!("type_names", Bar::PACKAGE);
-    assert_eq!("/type_names.Bar", Bar::type_url());
+    assert_eq!("Bar", foo::Bar::NAME);
+    assert_eq!("type_names", foo::Bar::PACKAGE);
+    assert_eq!("type_names.Foo.Bar", foo::Bar::full_name());
+    assert_eq!("tests/type_names.Foo.Bar", foo::Bar::type_url());
+
+    assert_eq!("Baz", Baz::NAME);
+    assert_eq!("type_names", Baz::PACKAGE);
+    assert_eq!("type_names.Baz", Baz::full_name());
+    assert_eq!("/type_names.Baz", Baz::type_url());
 }
