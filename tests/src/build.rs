fe5cd31f6df838d0a83c6b0bd307f20f1155be2e
Casper Meijn <casper@meijn.net>
2023-11-23 14:25:24 +0100

    feat: derive Copy trait for messages where possible
    Rust primitive types can be copied by simply copying the bits. Rust structs can also have this property by deriving the Copy trait.
    
    Automatically derive Copy for:
    - messages that only have fields with primitive types
    - the Rust enum for one-of fields
    - messages whose field type are messages that also implement Copy
    
    Generated code for Protobuf enums already derives Copy.

--- tests/src/build.rs
+++ tests/src/build.rs
@@ -88,6 +88,10 @@ fn main() {
         .unwrap();
 
     config
+        .compile_protos(&[src.join("derive_copy.proto")], includes)
+        .unwrap();
+
+    config
         .compile_protos(&[src.join("default_string_escape.proto")], includes)
         .unwrap();
 
