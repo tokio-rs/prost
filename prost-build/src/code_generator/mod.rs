5d4978b5a83f54ea631c09f889295fbede6532a0
gibbz00 <gabrielhansson00@gmail.com>
2024-04-19 11:15:11 +0200

    prost-build: extract `locations` module a separate file

--- prost-build/src/code_generator/mod.rs
+++ prost-build/src/code_generator/mod.rs
@@ -36,37 +36,8 @@ mod syntax;
 use syntax::Syntax;
 
 // IMPROVEMENT: would be nice to have this auto-generated
+mod locations;
 use locations::*;
-mod locations {
-    pub(super) struct FileDescriptorProtoLocations;
-
-    impl FileDescriptorProtoLocations {
-        pub const MESSAGE_TYPE: i32 = 4;
-        pub const ENUM_TYPE: i32 = 5;
-        pub const SERVICE: i32 = 6;
-    }
-
-    pub(super) struct DescriptorLocations;
-
-    impl DescriptorLocations {
-        pub const FIELD: i32 = 2;
-        pub const NESTED_TYPE: i32 = 3;
-        pub const ENUM_TYPE: i32 = 4;
-        pub const ONEOF_DECL: i32 = 8;
-    }
-
-    pub(super) struct EnumDescriptorLocations;
-
-    impl EnumDescriptorLocations {
-        pub const VALUE: i32 = 2;
-    }
-
-    pub(super) struct ServiceDescriptorProtoLocations;
-
-    impl ServiceDescriptorProtoLocations {
-        pub const METHOD: i32 = 2;
-    }
-}
 
 pub struct CodeGenerator<'a> {
     config: &'a mut Config,
