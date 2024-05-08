548739ac15b052f8d27e261064616b0398b32b20
Casper Meijn <casper@meijn.net>
2024-05-08 18:41:00 +0200

    chore: Release version 0.12.5 (#1050)
    _PROST!_ is a [Protocol Buffers](https://developers.google.com/protocol-buffers/) implementation for the [Rust Language](https://www.rust-lang.org/). `prost` generates simple, idiomatic Rust code from `proto2` and `proto3` files.
    
    This patch update brings new fixes:
    
    - fix: include_file should handle proto without package (#1002)
    - Place Config::format behind the format feature flag
    - Handle keyword `Self` after stripping enum type prefix (#998)
    
    ## Documentation
    - fix(readme): fix the link and badge for CI (#1049)
    
    ## Internal
    - style(codegen): `Syntax` to a separate file (#1029)
    - chore(codegen): extract c string escaping to a separate file (#1028)
    - style(prost-build): `CodeGenerator::boxed` method (#1019)
    - style(prost-build): Consolidate field data into struct (#1017)
    - style(prost-build): `BytesType and MapType` into a `collections` module. (#1030)
    - style(prost-build): Split `Config` and `Module` into a separate module and files (#1020)
    - style(prost-build): prost_path helper (#1018)
    - style: Fix toml indent (#1048)
    - style: Fix clippy warnings and enable clippy in CI (#1008)
    - build: Use git submodule to download protobuf sources (#1014)
    - ci: Add TOML validation with `taplo` (#1034)
    - tests: Create a separate tempdir for each test (#1044)
    - tests: Remove GoogleMessage3 and GoogleMessage4 benchmarks (#1037)
    - chore: Update internal crates to Rust edition 2021 (#1039)
    - chore: Update crate descriptions (#1038)
    - chore: Fix clippy checks in CI (#1032)
    - chore: Add Casper Meijn as author (#1025)

--- prost-build/src/lib.rs
+++ prost-build/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-build/0.12.2")]
+#![doc(html_root_url = "https://docs.rs/prost-build/0.12.5")]
 #![allow(clippy::option_as_ref_deref, clippy::format_push_string)]
 
 //! `prost-build` compiles `.proto` files into Rust.
