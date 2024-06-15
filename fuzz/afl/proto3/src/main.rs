05ac304f591d509fc5580a18cb0350d7d1f52c11
cynecx <me@cynecx.net>
2024-06-16 00:21:51 +0200

    Cleanup roundtrip testing

--- fuzz/afl/proto3/src/main.rs
+++ fuzz/afl/proto3/src/main.rs
@@ -1,10 +1,10 @@
 use afl::fuzz;
 
 use protobuf::test_messages::proto3::TestAllTypesProto3;
-use tests::roundtrip;
+use tests::roundtrip_proto;
 
 fn main() {
     fuzz!(|data: &[u8]| {
-        let _ = roundtrip::<TestAllTypesProto3>(data).unwrap_error();
+        let _ = roundtrip_proto::<TestAllTypesProto3>(data).unwrap_error();
     });
 }
