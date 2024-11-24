25fcc10607cd0924a9eb40445fe8c5e570e78d3c
cynecx <me@cynecx.net>
2024-11-24 04:53:49 +0100

    Add documentation for camelcase json name conversion

--- prost-derive/src/serde/utils.rs
+++ prost-derive/src/serde/utils.rs
@@ -4,6 +4,16 @@ pub trait ToProtoCamelCase: ToOwned {
 
 impl ToProtoCamelCase for str {
     fn to_proto_camel_case(&self) -> Self::Owned {
+        // Reference: https://protobuf.com/docs/language-spec#default-json-names
+        //
+        // If no json_name pseudo-option is present, the JSON name of the field will be
+        // the field's name converted to camelCase. To convert to camelCase:
+        //
+        // - Discard any trailing underscores (_)
+        // - When a leading or interior underscore is encountered, discard the underscore and
+        //   capitalize the next non-underscore character encountered.
+        // - Any other non-underscore and non-capitalized character is retained as is.
+        //
         let mut capitalize_next = false;
         let mut out = String::with_capacity(self.len());
         for chr in self.chars() {
