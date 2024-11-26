b594f41d9ffdb89ea78f68486321d1bf2d0d7371
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2024-11-26 18:24:22 +0000

    test: improve and expand encoded_len::overflow
    Add tests for derived Message::encoded_len implementations, organized
    in a separate module from the tests for functions in prost::encoding.
    
    Fix compilation of no_std tests.

--- tests/src/encoded_len/overflow.rs
+++ tests/src/encoded_len/overflow.rs
@@ -1,8 +1,25 @@
+//! Tests for integer overflow behavior of encoded_len* functions.
+//!
+//! Many of the tests in this module allocate and fill large amounts of RAM,
+//! so they should better be run in isolation and in a sufficiently
+//! memory-budgeted environment. Particularly on 32-bit platforms, the tests
+//! should not be run in parallel as the combined allocation requests can exceed
+//! the addressable memory.
+
 use prost::alloc::vec;
 
 #[cfg(target_pointer_width = "64")]
 fn verify_overflowing_encoded_len(actual: usize, expected: u64) -> bool {
-    actual as u64 == expected
+    if actual as u64 == expected {
+        true
+    } else {
+        cfg_if! {
+            if #[cfg(feature = "std")] {
+                eprintln!("expected {} but the function returned {}", expected, actual);
+            }
+        }
+        false
+    }
 }
 
 #[cfg(target_pointer_width = "32")]
@@ -10,94 +27,260 @@ fn verify_overflowing_encoded_len(actual: usize, _expected: u64) -> bool {
     // Tests calling this function are expected to panic on 32-bit platforms
     // before this check is called. Returning true here allows the
     // #[should_panic] tests to fail.
-    eprintln!("expected panic, but the function returned {actual}");
+    cfg_if! {
+        if #[cfg(feature = "std")] {
+            eprintln!("expected panic, but the function returned {actual}");
+        }
+    }
     true
 }
 
-mod bool {
-    use super::*;
-    use prost::encoding::{bool, MAX_TAG};
+mod field {
+    // Test encoded_len* functions in prost::encoding submodules for various field types.
 
-    #[test]
-    #[ignore = "allocates and fills about 666 MiB"]
-    #[cfg_attr(target_pointer_width = "32", should_panic)]
-    fn encoded_len_repeated_can_overflow_u32() {
-        let filler = false;
-        let filler_len = bool::encoded_len(MAX_TAG, &filler);
-        let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
-        let encoded_len = bool::encoded_len_repeated(MAX_TAG, &bomb32);
-        let expected_len = bomb32.len() as u64 * filler_len as u64;
-        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+    use super::*;
+
+    mod bool {
+        use super::*;
+        use prost::encoding::{bool, MAX_TAG};
+
+        #[test]
+        #[ignore = "allocates and fills about 666 MiB"]
+        #[cfg_attr(target_pointer_width = "32", should_panic)]
+        fn encoded_len_repeated_can_overflow_u32() {
+            let filler = false;
+            let filler_len = bool::encoded_len(MAX_TAG, &filler);
+            let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
+            let encoded_len = bool::encoded_len_repeated(MAX_TAG, &bomb32);
+            let expected_len = bomb32.len() as u64 * filler_len as u64;
+            assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+        }
+    }
+
+    mod int32 {
+        use super::*;
+        use prost::encoding::{encoded_len_varint, int32, key_len, MAX_TAG};
+
+        #[test]
+        #[ignore = "allocates and fills more than 1 GiB"]
+        #[cfg_attr(target_pointer_width = "32", should_panic)]
+        fn encoded_len_repeated_can_overflow_u32() {
+            let filler = -1i32;
+            let filler_len = int32::encoded_len(MAX_TAG, &filler);
+            let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
+            let encoded_len = int32::encoded_len_repeated(MAX_TAG, &bomb32);
+            let expected_len = bomb32.len() as u64 * filler_len as u64;
+            assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+        }
+
+        #[test]
+        #[ignore = "allocates and fills about 1.6 GiB"]
+        #[cfg_attr(target_pointer_width = "32", should_panic)]
+        fn encoded_len_packed_can_overflow_u32() {
+            let filler = -1i32;
+            let filler_len = encoded_len_varint(filler as u64);
+            let bomb_len = (u32::MAX as usize - key_len(MAX_TAG) - 5) / filler_len + 1;
+            let bomb32 = vec![filler; bomb_len];
+            let encoded_len = int32::encoded_len_packed(MAX_TAG, &bomb32);
+            let expected_data_len = bomb_len as u64 * filler_len as u64;
+            let expected_len = key_len(MAX_TAG) as u64
+                + encoded_len_varint(expected_data_len) as u64
+                + expected_data_len;
+            assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+        }
+    }
+
+    mod message {
+        use super::*;
+        use crate::encoded_len::proto;
+        use prost::encoding::{encoded_len_varint, key_len, message, MAX_TAG};
+
+        #[test]
+        #[cfg_attr(target_pointer_width = "32", should_panic)]
+        fn encoded_len_can_overflow_u32() {
+            let filler = proto::Empty {};
+            let filler_len = message::encoded_len(MAX_TAG, &filler);
+            let subcritical = vec![filler; u32::MAX as usize / filler_len];
+            let payload_len = subcritical.len() * filler_len;
+            assert_eq!(encoded_len_varint(payload_len as u64), 5);
+            assert!(key_len(MAX_TAG) + 5 >= filler_len);
+            let bomb32 = proto::Testbed {
+                repeated_empty: subcritical,
+                ..Default::default()
+            };
+            let encoded_len = message::encoded_len(MAX_TAG, &bomb32);
+            let expected_len = (key_len(MAX_TAG) + 5) as u64 + payload_len as u64;
+            assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+        }
+
+        #[test]
+        #[cfg_attr(target_pointer_width = "32", should_panic)]
+        fn encoded_len_repeated_can_overflow_u32() {
+            let filler = proto::Empty {};
+            let filler_len = message::encoded_len(MAX_TAG, &filler);
+            let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
+            let encoded_len = message::encoded_len_repeated(MAX_TAG, &bomb32);
+            let expected_len = bomb32.len() as u64 * filler_len as u64;
+            assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+        }
     }
 }
 
-/// The tests in this module each allocate and fill more than 1 GB in memory,
-/// so they should better be run in isolation and in a sufficiently
-/// memory-budgeted environment.
-mod int32 {
-    use super::*;
-    use prost::encoding::{encoded_len_varint, int32, key_len, MAX_TAG};
+mod derived {
+    // Test overflow behavior of Message::encoded_len method implementations
+    // generated by prost-derive.
 
-    #[test]
-    #[ignore = "allocates and fills more than 1 GiB"]
-    #[cfg_attr(target_pointer_width = "32", should_panic)]
-    fn encoded_len_repeated_can_overflow_u32() {
-        let filler = -1i32;
-        let filler_len = int32::encoded_len(MAX_TAG, &filler);
-        let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
-        let encoded_len = int32::encoded_len_repeated(MAX_TAG, &bomb32);
-        let expected_len = bomb32.len() as u64 * filler_len as u64;
-        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
-    }
-
-    #[test]
-    #[ignore = "allocates and fills about 1.6 GiB"]
-    #[cfg_attr(target_pointer_width = "32", should_panic)]
-    fn encoded_len_packed_can_overflow_u32() {
-        let filler = -1i32;
-        let filler_len = encoded_len_varint(filler as u64);
-        let bomb_len = (u32::MAX as usize - key_len(MAX_TAG) - 5) / filler_len + 1;
-        let bomb32 = vec![filler; bomb_len];
-        let encoded_len = int32::encoded_len_packed(MAX_TAG, &bomb32);
-        let expected_data_len = bomb_len as u64 * filler_len as u64;
-        let expected_len = key_len(MAX_TAG) as u64
-            + encoded_len_varint(expected_data_len) as u64
-            + expected_data_len;
-        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
-    }
-}
-
-mod message {
     use super::*;
     use crate::encoded_len::proto;
-    use prost::encoding::{encoded_len_varint, key_len, message, MAX_TAG};
+    use prost::alloc::collections::BTreeMap;
+    use prost::alloc::string::String;
+    use prost::alloc::vec::Vec;
+    use prost::encoding::{message, MAX_TAG};
+    use prost::Message;
+
+    // Initializes all scalar fields so as to give the largest possible
+    // encodings for these fields.
+    const FATTEST_SCALARS: proto::Testbed = proto::Testbed {
+        int32: -1,
+        int64: -1,
+        uint32: u32::MAX,
+        uint64: u64::MAX,
+        sint32: i32::MIN,
+        sint64: i64::MIN,
+        fixed32: 1,
+        fixed64: 1,
+        sfixed32: -1,
+        sfixed64: -1,
+        float: 1.0,
+        double: 1.0,
+        bool: true,
+        enumeration: proto::BadEnum::Long as i32,
+        string: String::new(),
+        bytes: Vec::new(),
+        packed_int32: vec![],
+        map: BTreeMap::new(),
+        repeated_empty: vec![],
+    };
+
+    const SCALAR_ENCODED_LEN_LIMITS: &[usize] = &[
+        10, // int32
+        10, // int64
+        5,  // uint32
+        10, // uint64
+        5,  // sint32
+        10, // sint64
+        4,  // fixed32
+        8,  // fixed64
+        4,  // sfixed32
+        8,  // sfixed64
+        4,  // float
+        8,  // double
+        1,  // bool
+        10, // enumeration
+    ];
+
+    #[test]
+    fn limited_length_scalar_encodings_are_accounted_for() {
+        assert_eq!(
+            FATTEST_SCALARS.encoded_len(),
+            SCALAR_ENCODED_LEN_LIMITS
+                .iter()
+                .cloned()
+                .map(|len| 1 + len)
+                .sum()
+        );
+    }
 
     #[test]
     #[cfg_attr(target_pointer_width = "32", should_panic)]
-    fn encoded_len_can_overflow_u32() {
+    fn encoded_len_can_overflow_u32_with_repeated_field() {
         let filler = proto::Empty {};
         let filler_len = message::encoded_len(MAX_TAG, &filler);
-        let subcritical = vec![filler; u32::MAX as usize / filler_len];
-        let payload_len = subcritical.len() * filler_len;
-        assert_eq!(encoded_len_varint(payload_len as u64), 5);
-        assert!(key_len(MAX_TAG) + 5 >= filler_len);
+        let supercritical =
+            vec![filler; (u32::MAX as usize - FATTEST_SCALARS.encoded_len()) / filler_len + 1];
+        let payload_len = supercritical.len() as u64 * filler_len as u64;
         let bomb32 = proto::Testbed {
-            repeated_empty: subcritical,
-            ..Default::default()
+            repeated_empty: supercritical,
+            ..FATTEST_SCALARS
         };
-        let encoded_len = message::encoded_len(MAX_TAG, &bomb32);
-        let expected_len = (key_len(MAX_TAG) + 5) as u64 + payload_len as u64;
+        let encoded_len = bomb32.encoded_len();
+        let expected_len = FATTEST_SCALARS.encoded_len() as u64 + payload_len as u64;
         assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
     }
 
     #[test]
     #[cfg_attr(target_pointer_width = "32", should_panic)]
-    fn encoded_len_repeated_can_overflow_u32() {
+    fn encoded_len_can_overflow_u32_with_string() {
         let filler = proto::Empty {};
         let filler_len = message::encoded_len(MAX_TAG, &filler);
-        let bomb32 = vec![filler; u32::MAX as usize / filler_len + 1];
-        let encoded_len = message::encoded_len_repeated(MAX_TAG, &bomb32);
-        let expected_len = bomb32.len() as u64 * filler_len as u64;
+        let padding =
+            vec![filler; (u32::MAX as usize - FATTEST_SCALARS.encoded_len()) / filler_len];
+        let padding_len = padding.len() * filler_len;
+        let bomb32 = proto::Testbed {
+            repeated_empty: padding,
+            string: " ".repeat(filler_len - 2 - 1),
+            ..FATTEST_SCALARS
+        };
+        let encoded_len = bomb32.encoded_len();
+        let expected_len =
+            FATTEST_SCALARS.encoded_len() as u64 + padding_len as u64 + filler_len as u64;
+        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+    }
+
+    #[test]
+    #[cfg_attr(target_pointer_width = "32", should_panic)]
+    fn encoded_len_can_overflow_u32_with_bytes() {
+        let filler = proto::Empty {};
+        let filler_len = message::encoded_len(MAX_TAG, &filler);
+        let padding =
+            vec![filler; (u32::MAX as usize - FATTEST_SCALARS.encoded_len()) / filler_len];
+        let padding_len = padding.len() * filler_len;
+        let bomb32 = proto::Testbed {
+            repeated_empty: padding,
+            bytes: b" ".repeat(filler_len - 2 - 1),
+            ..FATTEST_SCALARS
+        };
+        let encoded_len = bomb32.encoded_len();
+        let expected_len =
+            FATTEST_SCALARS.encoded_len() as u64 + padding_len as u64 + filler_len as u64;
+        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+    }
+
+    #[test]
+    #[cfg_attr(target_pointer_width = "32", should_panic)]
+    fn encoded_len_can_overflow_u32_with_packed_varint() {
+        let filler = proto::Empty {};
+        let filler_len = message::encoded_len(MAX_TAG, &filler);
+        let padding =
+            vec![filler; (u32::MAX as usize - FATTEST_SCALARS.encoded_len()) / filler_len];
+        let padding_len = padding.len() * filler_len;
+        let bomb32 = proto::Testbed {
+            repeated_empty: padding,
+            packed_int32: vec![0; filler_len - 2 - 1],
+            ..FATTEST_SCALARS
+        };
+        let encoded_len = bomb32.encoded_len();
+        let expected_len =
+            FATTEST_SCALARS.encoded_len() as u64 + padding_len as u64 + filler_len as u64;
+        assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
+    }
+
+    #[test]
+    #[cfg_attr(target_pointer_width = "32", should_panic)]
+    fn encoded_len_can_overflow_u32_with_map() {
+        let filler = proto::Empty {};
+        let filler_len = message::encoded_len(MAX_TAG, &filler);
+        let padding =
+            vec![filler; (u32::MAX as usize - FATTEST_SCALARS.encoded_len()) / filler_len];
+        let padding_len = padding.len() * filler_len;
+        let map = [(0, -1)].iter().cloned().collect();
+        let bomb32 = proto::Testbed {
+            repeated_empty: padding,
+            map,
+            ..FATTEST_SCALARS
+        };
+        let encoded_len = bomb32.encoded_len();
+        let expected_len = FATTEST_SCALARS.encoded_len() as u64 + padding_len as u64 + 14;
         assert!(verify_overflowing_encoded_len(encoded_len, expected_len));
     }
 }
