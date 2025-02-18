846c4529ae59432d9c17dd9448eb56d4f67c2964
Casper Meijn <casper@meijn.net>
2025-02-18 14:13:15 +0100

    fix(prost-build): Prevent repeated fields to be boxed (#1237)
    Repeated fields are stored in a `Vec`, and therefore they are already heap allocated.
    
    BREAKING CHANGE: A repeated field that is manually marked as boxed was typed as `Vec<Box<T>>`. Those fields are now simply typed as `Vec<T>` to prevent double indirection. The `boxed` configuration is effectively ignored for repeated fields.

--- tests/src/boxed_field.rs
+++ tests/src/boxed_field.rs
@@ -3,15 +3,18 @@ include!(concat!(env!("OUT_DIR"), "/boxed_field.rs"));
 use self::foo::OneofField;
 
 #[test]
-/// Confirm `Foo::bar` and `OneofField::BoxQux` is boxed by creating an instance
+/// - Confirm `Foo::bar` and `OneofField::BoxQux` is boxed by creating an instance
+/// - `Foo::boxed_bar_list` should not be boxed as it is a `Vec`, therefore it is already heap allocated
 fn test_boxed_field() {
     use alloc::boxed::Box;
-    let _ = Foo {
+    use alloc::vec::Vec;
+    let foo = Foo {
         bar: Some(Box::new(Bar {})),
         oneof_field: Some(OneofField::BoxQux(Box::new(Bar {}))),
+        boxed_bar_list: Vec::from([Bar {}]),
     };
     let _ = Foo {
-        bar: Some(Box::new(Bar {})),
         oneof_field: Some(OneofField::Baz("hello".into())),
+        ..foo
     };
 }
