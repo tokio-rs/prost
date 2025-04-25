0a576e75c75a8b0ff8ba9f57607d4c4828c6b22a
Casper Meijn <casper@meijn.net>
2025-04-25 14:47:56 +0200

    chore: Release version 0.14.0

--- prost-derive/src/lib.rs
+++ prost-derive/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-derive/0.13.5")]
+#![doc(html_root_url = "https://docs.rs/prost-derive/0.14.0")]
 // The `quote!` macro requires deep recursion.
 #![recursion_limit = "4096"]
 
