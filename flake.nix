5fd431d2a30ff2829283bab18f8e085578ec9b7d
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2025-07-11 03:39:27 -0300

    ci-cd(flake.nix): change to use fenix rust version manager and add another develop rust_minimum_version for tests

--- flake.nix
+++ flake.nix
@@ -4,17 +4,52 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
+        default_pkgs = with pkgs; [
+          protobuf
+          cmake
+          pkg-config
+          protobuf
+          curl
+          ninja
+        ];
       in
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ cargo rustc ];
-          buildInputs = with pkgs; [ pkg-config protobuf curl cmake ninja ];
-        };
-      });
+        devShells.default =
+          let
+            rustpkgs = fenix.packages.${system}.stable.defaultToolchain;
+          in
+          pkgs.mkShell {
+            packages = [
+              rustpkgs
+            ] ++ default_pkgs;
+          };
+        devShells."rust_minimum_version" =
+          let
+            rust_manifest = {
+              url = "https://static.rust-lang.org/dist/2023-08-03/channel-rust-1.71.1.toml";
+              flake = false;
+            };
+            rustpkgs = (fenix.packages.${system}.fromManifestFile rust_manifest).defaultToolchain;
+          in
+          pkgs.mkShell {
+            packages = [
+              rustpkgs
+            ] ++ default_pkgs;
+          };
+      }
+    );
 }
