b1acf6c90db3fbc69273e660f61a2c28b26746a2
Jerko Steiner <jerko.steiner@gmail.com>
2025-07-14 11:30:37 +0200

    Address PR comments

--- tests/prost-path/src/lib.rs
+++ tests/prost-path/src/lib.rs
@@ -1,1 +1,4 @@
+// This test ensures we can compile using re-exported dependencies as configured in
+// `build.rs`. Note that there's no direct dependency of `::prost` or `::prost-types` in
+// `Cargo.toml`.
 include!(concat!(env!("OUT_DIR"), "/prost_path.rs"));
