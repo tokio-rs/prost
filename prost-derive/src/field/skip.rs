26a3d25bcfe6a7497fccd4f400515b93399d7e5f
maxpowa <maxpowa1@gmail.com>
2025-09-21 17:05:31 -0700

    feat(derive): add support for skipping fields
    This will make prost ignore the field during serialization and deserialization. The field must implement the "Default" trait for this to work properly.

---
+++ prost-derive/src/field/skip.rs
@@ -0,0 +1,56 @@
+﻿use anyhow::{bail, Error};
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Meta};
+
+use crate::field::{set_bool, word_attr};
+
+#[derive(Clone)]
+pub struct Field;
+
+impl Field {
+    pub fn new(attrs: &[Meta]) -> Result<Option<Field>, Error> {
+        let mut skip = false;
+        let mut unknown_attrs = Vec::new();
+
+        for attr in attrs {
+            if word_attr("skip", attr) {
+                set_bool(&mut skip, "duplicate ignore attribute")?;
+            } else {
+                unknown_attrs.push(attr);
+            }
+        }
+
+        if !skip {
+            return Ok(None);
+        }
+
+        if !unknown_attrs.is_empty() {
+            bail!(
+                "unknown attribute(s) for ignored field: #[prost({})]",
+                quote!(#(#unknown_attrs),*)
+            );
+        }
+
+        Ok(Some(Field))
+    }
+
+    /// Returns a statement which non-ops, since the field is ignored.
+    pub fn encode(&self, _: TokenStream) -> TokenStream {
+        quote!()
+    }
+
+    /// Returns an expression which evaluates to the default value of the ignored field.
+    pub fn merge(&self, ident: TokenStream) -> TokenStream {
+        quote!(#ident.get_or_insert_with(::core::default::Default::default))
+    }
+
+    /// Returns an expression which evaluates to 0
+    pub fn encoded_len(&self, _: TokenStream) -> TokenStream {
+        quote!(0)
+    }
+
+    pub fn clear(&self, ident: TokenStream) -> TokenStream {
+        quote!(#ident = ::core::default::Default::default)
+    }
+}
