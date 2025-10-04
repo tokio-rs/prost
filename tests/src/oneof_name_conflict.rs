3eaaadc2d379231ef190ab541f4d30963d313056
Javier Cano <javier@jcanovi.com>
2025-10-04 11:52:41 -0700

    Fix clippy

--- tests/src/oneof_name_conflict.rs
+++ tests/src/oneof_name_conflict.rs
@@ -13,7 +13,7 @@ fn test_creation() {
 
     let _ = oneof_name_conflict::EnumAndOneofConflict {
         r#type: Some(
-            oneof_name_conflict::enum_and_oneof_conflict::TypeOneOf::TypeThree(
+            oneof_name_conflict::enum_and_oneof_conflict::TypeOneOf::Three(
                 oneof_name_conflict::enum_and_oneof_conflict::TypeThree {
                     field: oneof_name_conflict::enum_and_oneof_conflict::Type::Type1.into(),
                 },
